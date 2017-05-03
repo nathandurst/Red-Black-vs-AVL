6eb9008b090aff7a354239a0b57041864fbfac23
Nathan Durst <nathandurst3@gmail.com>
2017-05-03 10:57:15 -0500

    Update Source.cpp
    removed commented code

--- Red-Black v AVL/Red-Black v AVL/Source.cpp
+++ Red-Black v AVL/Red-Black v AVL/Source.cpp
@@ -23,22 +23,8 @@ void main()
 		int value = rand() % 100;
 		rb.insert(value);
 	}
-	
-	/*rb.insert(10);
-	rb.insert(20);
-	rb.insert(5);
-	rb.insert(16);
-	rb.insert(35);
-	rb.insert(100);
-	rb.insert(40);
-	rb.insert(2);*/
 
 	outfile << "Nathan Durst" << endl << endl;
-	/*outfile << "the tree data in order is:" << endl;
-	rb.inOrder(rb.root, outfile);
-	outfile << endl << endl;*/
-
-	//rb.GraphVizOut(outfile);
 	outfile << endl << endl;
 	
 	outfile<< "With " << insertCounter << " nodes inserted\n";
